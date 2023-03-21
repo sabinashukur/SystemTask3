d5395acd22a1ffa0b331d28b160b5960f46aeaf8
Sabina <sabinashukur8@gmail.com>
2023-03-22 00:46:06 +0400

    Add project files.

---
+++ SystemTask3/AesOperation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SystemTask3;
+
+public class AesOperation
+{
+    public static byte[] EncryptStringToBytes(string key, string plainText)
+    {
+        byte[] iv = new byte[16];
+
+        using Aes aes = Aes.Create();
+
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = iv;
+
+        ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+        using MemoryStream memoryStream = new MemoryStream();
+
+        using CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
+        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+        {
+            streamWriter.Write(plainText);
+        }
+
+        return memoryStream.ToArray();
+    }
+
+    public static string DecryptString(string key, string cipherText)
+    {
+        byte[] iv = new byte[16];
+        byte[] buffer = Convert.FromBase64String(cipherText);
+
+        using Aes aes = Aes.Create();
+
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = iv;
+
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        using MemoryStream memoryStream = new MemoryStream(buffer);
+
+        using CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+
+        using (StreamReader streamReader = new StreamReader(cryptoStream))
+        {
+            return streamReader.ReadToEnd();
+        }
+    }
+}
---
+++ SystemTask3/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace SystemTask3;
+
+public partial class MainWindow : Window
+{
+    private readonly OpenFileDialog _openFileDialog;
+    private readonly CancellationTokenSource _cts;
+
+    //private bool _isVisiblePasswordBox;
+    //public bool IsVisiblePasswordBox
+    //{
+    //    get { return tbox_fileName.Text.Length > 0; }
+    //    set { _isVisiblePasswordBox = value; }
+    //}
+
+    public bool IsVisiblePasswordBox => tbox_fileName.Text.Length > 0;
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+        _openFileDialog = new();
+        _cts = new();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        _openFileDialog.Filter = "Txt files (*.txt)|*.txt";
+        _openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+    }
+
+    private void btn_chooseFile_Click(object sender, RoutedEventArgs e)
+    {
+        if (_openFileDialog.ShowDialog() == true)
+        {
+            tbox_fileName.Text = _openFileDialog.FileName;
+        }
+    }
+
+    private void btn_start_Click(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(tbox_fileName.Text))
+        {
+            MessageBox.Show("File path cannot be empty");
+            return;
+        }
+
+        if (!File.Exists(tbox_fileName.Text))
+        {
+            MessageBox.Show("File doesn't exist");
+            return;
+        }
+
+        if (tbox_password.Text.Length < 16)
+        {
+            MessageBox.Show("Password must contain 16 characters");
+            return;
+        }
+
+        if (rdbutton_encrpyt.IsChecked == false || rdbutton_decrpyt.IsChecked == false)
+        {
+            MessageBox.Show("Choose action");
+            return;
+        }
+
+        EncryptAndWrite(_cts.Token);
+
+    }
+
+    private void EncryptAndWrite(CancellationToken token)
+    {
+        var plainText = File.ReadAllText(tbox_fileName.Text);
+
+        var encryptedBytes = AesOperation.EncryptStringToBytes(tbox_password.Text, plainText);
+
+        var dividedBytesArray = encryptedBytes.Chunk(10);
+        using FileStream fs = new FileStream(tbox_fileName.Text, FileMode.Truncate);
+
+        foreach (var bytesArray in dividedBytesArray)
+        {
+            fs.Write(bytesArray, 0, bytesArray.Length);
+            //Thread.Sleep(100);
+        }
+    }
+
+    private void btn_cancel_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
